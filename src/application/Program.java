b3d7e771ce2c250ae1fe70c3bcb8252e2af8896a
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-03-08 19:44:20 -0300

    Exercicio para praticar exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,51 +1,42 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
-import model.entities.Reservation;
-import model.exceptions.DomainException;
+import model.entities.Account;
+import model.exceptions.BusinessException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		try {
-			System.out.print("Room number: ");
-			int number = sc.nextInt();
-			System.out.print("Check-in date (dd/MM/yyyy): ");
-			Date checkIn = sdf.parse(sc.next());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			Date checkOut = sdf.parse(sc.next());
-	
-			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-	
-			System.out.println();
-			System.out.println("Enter data to update the reservation: ");
-			System.out.print("Check-in date (dd/MM/yyyy): ");
-			checkIn = sdf.parse(sc.next());
-			System.out.print("Check-out date (dd/MM/yyyy): ");
-			checkOut = sdf.parse(sc.next());
-	
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println("Reservation: " + reservation);
-		}
-		catch (ParseException e) {
-			System.out.println("Invalid date format");
-		}
-		catch (DomainException e) {
-			System.out.println("Error in reservation" + e.getMessage());
-		}
-		catch (RuntimeException e) {
-			System.out.println("Unexpected error");
-		}
+		System.out.println("Enter account data:");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account account = new Account(number, holder, balance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
 		
+		try {
+			account.withdraw(amount);
+			System.out.printf("New balance: %.2f%n", account.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+
 		sc.close();
 	}
 
